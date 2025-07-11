07fd932100d3785eaae76347c0896eabdaf3919d
Krzysztofpan <jujujj2007@gmail.com>
2025-07-11 15:22:34 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import MainContent from '@/components/MainContent'
 
 export default async function Home() {
-  const res = await fetch(`/api/entries`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/entries`, {
     cache: 'no-store',
   })
 
